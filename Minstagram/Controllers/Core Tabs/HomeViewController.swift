6e215d1c78be35bb5e0debf8e778dabb6bae560c
Jin <m.davit@live.com>
2023-03-28 22:04:12 +0400

    First Commit

---
+++ Minstagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minstagram
+//
+//  Created by Davit Margaryan on 28.03.23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
